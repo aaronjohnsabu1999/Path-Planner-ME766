cc9765f8fdd1e8b395164f91a9834caee3a82d13
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2021-05-16 21:36:47 +0530

    Updated codes, report, and submission

--- submission/Codes/Dijkstra/Dijkstra.cpp
+++ submission/Codes/Dijkstra/Dijkstra.cpp
@@ -8,66 +8,56 @@
 # define INF 0x3f3f3f3f
 using namespace std;
 
-// This class represents a directed graph using
-// adjacency list representation
+// This class represents a directed graph
+// using adjacency list representation
 class Graph
 {
-	int V; // No. of vertices
-
-	// In a weighted graph, we need to store vertex
-	// and weight pair for every edge
-	//list< pair<int, int> > *adj;
-
+  int V;  // No. of vertices
 public:
-	list< pair<int, int> > *adj;
+  list< pair<int, int> > *adj;  // List of all edge pairs
+  vector<int> path;
 
-	vector<int> path;
-
-	Graph(int V); // Constructor
-
-	// function to add an edge to graph
-	void addEdge(int u, int v, int w);
-
-	// prints shortest path from s
-	void Dijkstras_Algo(int s, int dest);
-	void Dijkstras_Algo_omp(int s, int dest);
-	void BellmanFord_Algo(int s);
-	void FloydWarshall_Algo(int s);
+  Graph(int V);
+  int vertNum();
+  void addEdge(int u, int v, int w);
+  void Dijkstras_Algo(int s, int dest);
+  void Dijkstras_Algo_omp(int s, int dest);
+  void BellmanFord_Algo(int s);
+  void FloydWarshall_Algo(int s);
 };
-
-// Allocates memory for adjacency list
+// Return number of vertices in the graph
+int Graph::vertNum()
+{
+  return this->V;
+}
+// Constructor: Allocate memory for adjacency list
 Graph::Graph(int V)
 {
-	this->V = V;
-	adj = new list< pair<int, int> >[V];
+  this->V = V;
+  adj = new list< pair<int, int> >[V];
 }
-
+// Add an edge to the graph
 void Graph::addEdge(int u, int v, int w)
 {
-	adj[u].push_back(make_pair(v, w));
-	adj[v].push_back(make_pair(u, w));
+  adj[u].push_back(make_pair(v, w));
+  adj[v].push_back(make_pair(u, w));
 }
-
-// Prints shortest paths from src to all other vertices.. dest is the maximum weigth on a edge in the graph
-
+// Shortest paths from src to dest or to all other vertices
 void Graph::Dijkstras_Algo(int src, int dest)
 {
-	// Create a set to store vertices that are being
-	// prerocessed
+	// Create a set to store vertices that are being preprocessed
 	set< pair<int, int> > setds;
 
-	// Create a vector for distances and initialize all
-	// distances as infinite (INF)
+	// Create a vector for distances and initialize all distances as infinite (INF)
 	vector<int> dist(V, INF);
 	vector<int> parent(V, 0); 
 	parent[src] = -1;
-	// Insert source itself in Set and initialize its
-	// distance as 0.
+	// Insert source itself in Set and initialize its distance as 0.
 	setds.insert(make_pair(0, src));
 	dist[src] = 0;
 
-	/* Looping till all shortest distance are finalized
-	then setds will become empty */
+	// Looping till all shortest distance are finalized
+	// then setds will become empty
 	double start, end;
 	start = omp_get_wtime();	
 	while (!setds.empty())
@@ -87,8 +77,7 @@ void Graph::Dijkstras_Algo(int src, int dest)
 		list< pair<int, int> >::iterator i;
 		for (i = adj[u].begin(); i != adj[u].end(); ++i)
 		{
-			// Get vertex label and weight of current adjacent
-			// of u.
+			// Get vertex label and weight of current adjacent of u.
 			int v = (*i).first;
 			int weight = (*i).second;
 
@@ -104,7 +93,6 @@ void Graph::Dijkstras_Algo(int src, int dest)
 					we would never reach here. */
 				if (dist[v] != INF)
 					setds.erase(setds.find(make_pair(dist[v], v)));
-
 				// Updating distance of v
 				dist[v] = dist[u] + weight;
 				setds.insert(make_pair(dist[v], v));
@@ -113,191 +101,15 @@ void Graph::Dijkstras_Algo(int src, int dest)
 	}
 	end = omp_get_wtime();
 	printf("Serial Running time: %f ms\n", (end - start)*1000);
-	//vector<vector<int>> vec(V);
-	// Print shortest distances stored in dist[]
-	//printf("Vertex Distance from Source\n");
 	int j;
 	for (int i = 0; i < V; ++i){
-		//printf("%d \t\t %d\n", i, dist[i]);
 		j = i+ 1;
 		ofstream myfile ("USA_NY_out.txt", ios::app);
-		//ofstream myfile ("tmp_out.txt", ios::app);
 		myfile << j << " \t\t " << dist[i] << endl;
 		myfile.close();		
-		//j = i;
-		//printf("%d  ", j);
-		//while(parent[j]!=-1){
-		//	vec[i].push_back(j);
-		//	j = parent[j];
-			//printf("%d  ", j);
-		//}
-		//printf("\n");
-		//reverse(vec[i].begin(), vec[i].end());
-		//for(int k=0; k< vec[i].size(); k++){
-		//	printf("%d  ", vec[i][k]);
-		//}
-		//printf("\n");		
 	}
-	//path = vec[dest];
-	//return_path(vec[dest]);
-	/*vector<int> z = vec[dest];
-	for(int it=0; it<path.size();it++){
-		printf("%d  ", path[it]);
-	}
-	return z;*/
 }
 
-
-// void Graph::Dijkstras_Algo_omp_old(int src, int dest)
-// {
-// 	// Create a set to store vertices that are being
-// 	// prerocessed
-// 	set< pair<int, int> > setds;
-
-// 	// Create a vector for distances and initialize all
-// 	// distances as infinite (INF)
-// 	vector<int> dist(V, INF);
-// 	vector<int> parent(V, 0); 
-// 	parent[src] = -1;
-// 	// Insert source itself in Set and initialize its
-// 	// distance as 0.
-// 	setds.insert(make_pair(0, src));
-// 	dist[src] = 0;
-
-// 	/* Looping till all shortest distance are finalized
-// 	then setds will become empty */
-// 	std::vector<list< pair<int, int> >::iterator> element;
-// 	int count = 0;
-// 	int count1 = 0;
-// 	double start, end;
-// 	start = omp_get_wtime();
-// 	while (!setds.empty())
-// 	{
-// 		cout<< count++ <<endl;
-// 		// The first vertex in Set is the minimum distance
-// 		// vertex, extract it from set.
-// 		pair<int, int> tmp = *(setds.begin());
-// 		setds.erase(setds.begin());
-
-// 		// vertex label is stored in second of pair (it
-// 		// has to be done this way to keep the vertices
-// 		// sorted distance (distance must be first item
-// 		// in pair)
-// 		int u = tmp.second;
-
-// 		// 'i' is used to get all adjacent vertices of a vertex
-// 		list< pair<int, int> >::iterator i;
-		
-// 		int v;
-// 		int weight;
-
-// 		element.clear();
-// 		for (i = adj[u].begin(); i != adj[u].end(); ++i)
-// 		{
-// 			element.push_back(i);
-// 		}
-
-// 		i = element.at(0);
-// 		int j;
-// 		// std::vector<int> a1;
-// 		// std::vector<int> a2;
-// 		int *b1 = new int[element.size()];
-// 		int *b2 = new int[element.size()];
-// 		count1 = 0;
-
-// 		#pragma omp parallel for schedule(runtime) private(i, j, v, weight) shared(setds,dist,parent,u,b1,b2,count1)
-// 		//#pragma omp parallel for shared(setds, a1, a2)
-// 		//#pragma omp parallel for schedule(runtime) private(j)
-// 		for (j = 0; j < element.size(); ++j)
-// 		{
-// 			//cout<< j << endl;
-// 			// Get vertex label and weight of current adjacent
-// 			// of u.
-			
-// 			//cout<< "1" << endl;
-// 			i = element.at(j);
-// 			//cout<< "2" << endl;
-// 			v = (*i).first;
-// 			//cout<< "3" << endl;
-// 			weight = (*i).second;
-// 			//cout<< "4" << endl;
-
-// 			// If there is shorter path to v through u.
-// 			if (dist[v] > dist[u] + weight)
-// 			{
-// 				//parent[v] = u;
-// 				/* If distance of v is not INF then it must be in
-// 					our set, so removing it and inserting again
-// 					with updated less distance.
-// 					Note : We extract only those vertices from Set
-// 					for which distance is finalized. So for them,
-// 					we would never reach here. */
-// 				if (dist[v] != INF)
-// 					setds.erase(setds.find(make_pair(dist[v], v)));
-
-// 				// Updating distance of v
-// 				dist[v] = dist[u] + weight;
-// 				#pragma omp critical
-// 				{
-// 					//setds.insert(make_pair(dist[v], v));
-// 					// a1.push_back(dist[v]);
-// 					// a2.push_back(v);
-// 					b1[count1] = dist[v];
-// 					b2[count1] = v;
-// 					count1++;
-// 				}
-// 				//setds.insert(make_pair(3, 4));
-// 			}
-			
-// 		}
-// 		#pragma omp barrier
-		
-// 		// for(j=0;j<a1.size();j++){
-// 		// 	setds.insert(make_pair(a1.at(j), a2.at(j)));
-// 		// }
-		
-// 		for(j=0;j<count1;j++){
-// 			setds.insert(make_pair(b1[j], b2[j]));
-// 		}
-// 		delete[] b1;
-// 		delete[] b2;	
-
-// 	}
-// 	end = omp_get_wtime();
-// 	printf("Parallel Running time: %f ms\n", (end - start)*1000);
-// 	// vector<vector<int>> vec(V);
-// 	// Print shortest distances stored in dist[]
-// 	// printf("Vertex Distance from Source\n");
-// 	int j;
-// 	for (int i = 0; i < V; ++i){
-// 		//printf("%d \t\t %d\n", i, dist[i]);
-// 		j = i+ 1;
-// 		ofstream myfile ("USA_NY_parallel_out.txt", ios::app);
-// 		//ofstream myfile ("tmp_parallel_out.txt", ios::app);
-// 		myfile << j << " \t\t " << dist[i] << endl;
-// 		myfile.close();		
-// 		//j = i;
-// 		//printf("%d  ", j);
-// 		//while(parent[j]!=-1){
-// 		//	vec[i].push_back(j);
-// 		//	j = parent[j];
-// 			//printf("%d  ", j);
-// 		//}
-// 		//printf("\n");
-// 		//reverse(vec[i].begin(), vec[i].end());
-// 		//for(int k=0; k< vec[i].size(); k++){
-// 		//	printf("%d  ", vec[i][k]);
-// 		//}
-// 		//printf("\n");		
-// 	}
-// 	//path = vec[dest];
-// 	//return_path(vec[dest]);
-// 	/*vector<int> z = vec[dest];
-// 	for(int it=0; it<path.size();it++){
-// 		printf("%d  ", path[it]);
-// 	}
-// 	return z;*/
-// }
 int search(int *a, int item, int low, int high)
 {
     if (high <= low)
@@ -315,7 +127,6 @@ int search(int *a, int item, int low, int high)
     return search(a, item, low,
                         mid - 1);
 }
-
 
 void sort(int *a1, int *a2, int index)
 {
@@ -341,16 +152,6 @@ void sort(int *a1, int *a2, int index)
 
 }
 
-// void erase(int start, int *a1, int *a2, int *ptr)
-// {
-// 	for(int i=start; i < *ptr; i++){
-// 		a1[i] = a1[i+1];
-// 		a2[i] = a2[i+1];
-// 	}
-// 	*ptr = *ptr - 1;
-// }
-
-
 void erase(int start, int *a1, int *a2, int index)
 {
 	for(int i=start; i < index + 2; i++){
@@ -358,106 +159,6 @@ void erase(int start, int *a1, int *a2, int index)
 		a2[i] = a2[i+1];
 	}
 }
-
-
-// void Graph::Dijkstras_Algo_omp(int src, int dest)
-// {
-// 	int vertices = this->V;
-// 	int *dist = new int[vertices];
-// 	for(int i=0; i<vertices; i++){
-// 		dist[i] = INF;
-// 	}
-// 	dist[src] = 0;
-// 	int n_thread,my_thread;
-
-// 	int *a1 = new int[vertices];//conatins vertices with unverified min
-// 	int *a2 = new int[vertices];//distance for above vertex
-// 	int index = -1;//last valid index for above two
-// 	index++;
-// 	a1[index] = src;
-// 	a2[index] = dist[src];
-// 	//sort(a1,a2,index);
-// 	std::vector<list< pair<int, int> >::iterator> element;
-// 	int tmp;
-// 	double start, end;
-// 	start = omp_get_wtime();
-
-// 	while(index>=0)
-// 	{
-// 		sort(a1, a2, index);
-// 		int u = a1[0];
-// 		//erase(0,a1,a2,&index);
-// 		erase(0,a1,a2,index);
-// 		index--;
-
-// 		int j, v, weight;
-// 		list< pair<int, int> >::iterator i;
-
-// 		element.clear();
-// 		for (i = adj[u].begin(); i != adj[u].end(); ++i)
-// 		{
-// 			element.push_back(i);
-// 		}
-
-// 		//i = element.at(0);
-
-// 		//#pragma omp parallel for schedule(runtime) private(i, j, v, weight, tmp) shared(dist,u,a1,a2,index)
-// 		for(j=0; j < element.size(); j++)
-// 		{
-// 			i = element.at(j);
-
-// 			v = (*i).first;
-
-// 			weight = (*i).second;
-
-// 			if(dist[v] > dist[u] + weight)
-// 			{
-// 				if(dist[v] != INF)
-// 				{
-// 					//#pragma omp critical
-// 					{
-// 						tmp = search(a1, v, 0, index);
-// 						//erase(tmp, a1, a2, &index);
-// 						erase(tmp, a1, a2, index);
-// 						index--;
-// 					}
-// 				}
-
-// 				dist[v] = dist[u] + weight;
-// 				//#pragma omp barrier
-
-// 				//#pragma omp critical
-// 				{
-// 					index++;
-// 					a1[index] = v;
-// 					a2[index] = dist[v];
-// 				}
-// 				//#pragma omp barrier
-				
-// 				//#pragma omp critical
-// 				{
-// 					sort(a1, a2, index);
-// 				}
-// 			}
-
-// 		}
-
-// 	} 
-
-// 	end = omp_get_wtime();
-// 	printf("Parallel Running time: %f ms\n", (end - start)*1000);
-	
-// 	int j;
-// 	for (int i = 0; i < V; ++i){
-// 		j = i+ 1;
-// 		ofstream myfile ("USA_NY_parallel_out.txt", ios::app);
-// 		//ofstream myfile ("tmp_parallel_out.txt", ios::app);
-// 		myfile << j << " \t\t " << dist[i] << endl;
-// 		myfile.close();			
-// 	}
-
-// }
-
 
 void Graph::Dijkstras_Algo_omp(int src, int dest)
 {
@@ -494,7 +195,6 @@ void Graph::Dijkstras_Algo_omp(int src, int dest)
 		element.push_back(partial_element);		
 	}
 
-
 	for (k = adj[src].begin(); k != adj[src].end(); ++k)
 	{
 		v = (*k).first;
@@ -508,17 +208,15 @@ void Graph::Dijkstras_Algo_omp(int src, int dest)
 	double start, end;
 	start = omp_get_wtime();
 
-	#pragma omp parallel private(weight, k, first, last, thread, step, min_d, min_node) shared(connected,d,dist,v,n_thread,adj,element,check)
+	for(step=0;step<vertices;step++)
 	{
-		thread = omp_get_thread_num();
-		n_thread = omp_get_num_threads();
-		first = (thread*vertices)/n_thread;
-		last = ((thread+1)*vertices)/n_thread;
-
-		for(step=1;step<vertices;step++)
-		//while(check)
-		//for(step=first;step<last;step++)
+		#pragma omp parallel private(i, weight, k, first, last, thread, step, min_d, min_node) shared(connected,d,dist,v,n_thread,element,check)
 		{
+			thread = omp_get_thread_num();
+			n_thread = omp_get_num_threads();
+			first = (thread*vertices)/n_thread;
+			last = ((thread+1)*vertices)/n_thread;
+
 			#pragma omp single
 			{
 				d = INF;
@@ -554,62 +252,29 @@ void Graph::Dijkstras_Algo_omp(int src, int dest)
 					connected[v] = true;
 				}
 			}
+			#pragma omp barrier					
+		}
 
-			#pragma omp barrier
-
-			if(v != -1)
+		if(v != -1)
+		{
+			int j;
+			#pragma omp parallel for schedule(runtime) private(i, j, weight, k) shared(dist,v,element)
+			for(j =0; j < element[v].size(); j++)
 			{
-				for (int i = first; i <= last; i++ )
+				k = element[v].at(j);
+				i = (*k).first;
+				if((!connected[i]))
 				{
-					if ( !connected[i] )
+					weight = (*k).second;
+					if(dist[i] > dist[v] + weight)
 					{
-						int j;
-						//for(k = adj[v].begin(); k != adj[v].end(); ++k)
-						for(j = 0; j < element[v].size(); ++j)
-						{
-							k = element[v].at(j);
-							if(i == (*k).first)
-							{
-								weight = (*k).second;
-								if(weight < INF)
-								{
-									if(dist[i] > dist[v] + weight)
-									{
-										dist[i] = dist[v] + weight;
-									}
-								}
-							}
-						}
-					  // if ( ohd[mv][i] < i4_huge )
-					  // {
-					  //   if ( mind[mv] + ohd[mv][i] < mind[i] )  
-					  //   {
-					  //     mind[i] = mind[mv] + ohd[mv][i];
-					  //   }
-					  // }
+						dist[i] = dist[v] + weight;
 					}
-				}				
-			}
-
-			#pragma omp barrier
-
-			// check = false;
-			// #pragma omp single
-			// {
-			// 	for(int i=0; i< vertices;i++)
-			// 	{
-			// 		if(connected[i] = false)
-			// 		{
-			// 			check = true;
-			// 		}
-			// 	}
-			// }
-
-			// #pragma omp barrier
-
+				}
+			}				
 		}
 	}
-
+    
 	end = omp_get_wtime();
 	printf("Parallel Running time: %f ms\n", (end - start)*1000);	
 
@@ -619,186 +284,52 @@ void Graph::Dijkstras_Algo_omp(int src, int dest)
 	for (int i = 0; i < V; ++i){
 		j = i+ 1;
 		ofstream myfile ("USA_NY_parallel_out.txt", ios::app);
-		//ofstream myfile ("tmp_parallel_out.txt", ios::app);
 		myfile << j << " \t\t " << dist[i] << endl;
 		myfile.close();			
 	}
-
 	delete[] dist;
-
 }
-
-
 void Graph::BellmanFord_Algo(int src)
 {
-
-
+  
 }
-
 void Graph::FloydWarshall_Algo(int src)
 {
-
-
+  
 }
-
 
 int main()
 {
-	/*
-	int V = 9;
-	Graph g(V);
-
-	// making a graph
-	g.addEdge(0, 1, 4);
-	g.addEdge(0, 7, 8);
-	g.addEdge(1, 2, 8);
-	g.addEdge(1, 7, 11);
-	g.addEdge(2, 3, 7);
-	g.addEdge(2, 8, 2);
-	g.addEdge(2, 5, 4);
-	g.addEdge(3, 4, 9);
-	g.addEdge(3, 5, 14);
-	g.addEdge(4, 5, 10);
-	g.addEdge(5, 6, 2);
-	g.addEdge(6, 7, 1);
-	g.addEdge(6, 8, 6);
-	g.addEdge(7, 8, 7);
-
-	return 0;
-	*/
-// 	fstream disFile;
-// 	disFile.open("../USA-road-d_NY.txt");
+	fstream disFile;
+	disFile.open("USA-road-d_NY.txt");
 	
-// 	vector<vector<int>> vect;
+	vector< vector<int> > vect;
 	
-// 	if (disFile.is_open()){ 
-// 		string tp;
-// //		int count =15;	
-// 		while(getline(disFile, tp)){ 
-// //			getline(disFile, tp);
-// 			vector<int> pair;
-// 			if(tp[0] == 'a'){
-// 				int a;
-// 				string str(tp.begin()+1, tp.begin()+tp.size() );
-// 				stringstream ss (str);
-// 				while ((ss >> a))
-// 					pair.push_back (a);
-// 				vect.push_back(pair);
-// 				//cout<<vect.size();
-// 			}
-// //			count --;
-// 		}
-// 		disFile.close(); 
-//    }
-   
-// //    for (int i = 0; i < vect.size(); i++) {
-// //        for (int j = 0; j < vect[i].size(); j++)
-// //            cout << vect[i][j] << " ";
-// //        cout << endl;
-// //    }
+	if (disFile.is_open()){ 
+		string tp;
+		while(getline(disFile, tp)){ 
+			vector<int> pair;
+			if(tp[0] == 'a'){
+				int a;
+				string str(tp.begin()+1, tp.begin()+tp.size() );
+				stringstream ss (str);
+				while ((ss >> a))
+					pair.push_back (a);
+				vect.push_back(pair);
+			}
+		}
+		disFile.close(); 
+    }
+    
+	Graph g(264346);
 
-// 	Graph g(264346);
-
-// 	cout<<"Number of edges: "<<vect.size()<<'\n';
+	cout<<"Number of edges: "<<vect.size()<<'\n';
 	
-//     for (int i = 0; i < vect.size(); i++) {
-//     	g.addEdge(vect[i][0]-1,vect[i][1]-1,vect[i][2]);
-//     }
+    for (int i = 0; i < vect.size(); i++) {
+    	g.addEdge(vect[i][0]-1,vect[i][1]-1,vect[i][2]);
+    }
 	
-// 	g.Dijkstras_Algo(0, 100);
-// 	g.Dijkstras_Algo_omp(0, 100);
-	
-	// int V = 25;
-	// Graph g(V);
-	// int i,j;
-	// for(int x=0; x<5; x++){
-	// 	for(int y=0; y<5;y++){
-	// 		if(g.adj[(5*y+x)].empty()==true){
-	// 		if(y==0){
-	// 			if(x==0){
-	// 				g.addEdge(0,1,1);
-	// 				g.addEdge(0,5,1);
-	// 			}
-	// 			else if(x==4){
-	// 				g.addEdge(4,3,1);
-	// 				g.addEdge(4,9,1);					
-	// 			}
-	// 			else{
-	// 				i = 5*y+x;
-	// 				j = 5*(y+1)+x;
-	// 				g.addEdge(i,j,1);
-	// 				j = i+1;
-	// 				g.addEdge(i,j,1);
-	// 				j = i-1;
-	// 				g.addEdge(i,j,1);
-	// 			}
-	// 		}
-	// 		else if(y==4){
-	// 			if(x==0){
-	// 				g.addEdge(20,21,1);
-	// 				g.addEdge(20,15,1);
-	// 			}
-	// 			else if(x==4){
-	// 				g.addEdge(24,23,1);
-	// 				g.addEdge(24,19,1);					
-	// 			}
-	// 			else{
-	// 				i = 5*y+x;
-	// 				j = 5*(y-1)+x;
-	// 				g.addEdge(i,j,1);
-	// 				j = i+1;
-	// 				g.addEdge(i,j,1);
-	// 				j = i-1;
-	// 				g.addEdge(i,j,1);
-	// 			}				
-	// 		}
-	// 		else if(x==0){
-	// 			i = 5*y+x;
-	// 			j = 5*(y-1)+x;
-	// 			g.addEdge(i,j,1);
-	// 			j = 5*(y+1)+x;
-	// 			g.addEdge(i,j,1);
-	// 			j = i+1;
-	// 			g.addEdge(i,j,1);				
-	// 		}
-	// 		else if(x==4){
-	// 			i = 5*y+x;
-	// 			j = 5*(y-1)+x;
-	// 			g.addEdge(i,j,1);
-	// 			j = 5*(y+1)+x;
-	// 			g.addEdge(i,j,1);
-	// 			j = i-1;
-	// 			g.addEdge(i,j,1);				
-	// 		}
-	// 		else{
-	// 			i = 5*y+x;
-	// 			//if(g.adj[i].empty()==true){
-	// 			j = 5*(y-1)+x;
-	// 			g.addEdge(i,j,1);
-	// 			j = 5*(y+1)+x;
-	// 			g.addEdge(i,j,1);
-	// 			j = i-1;
-	// 			g.addEdge(i,j,1);
-	// 			j = i+1;
-	// 			g.addEdge(i,j,1);
-	// 			//}	
-	// 		}
-	// 		}			
-	// 	}
-	// }
-
-	// //g.Dijkstras_Algo(0, 5);
-	// g.Dijkstras_Algo_omp(0, 5);
-
-	int V = 10000;
-	int E = 50000;
-	Graph g(V);
-	g.addEdge(0,2,1);
-	for (int j = 1; j < E; j++) {
-		g.addEdge(rand()%(V) + 0, rand()%(V) + 0, rand()%(50 - 1 + 1) + 1);
-	}
-	g.Dijkstras_Algo(0, 5);	
-	g.Dijkstras_Algo_omp(0, 5);
-
+	g.Dijkstras_Algo(0, 100);
+	g.Dijkstras_Algo_omp(0, 100);
 	return 0;
 }
